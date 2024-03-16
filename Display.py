1a61d6c41c1ab42e49c0fee3f8722041909de3a8
Samuel Lee <leesamuel423@gmail.com>
2024-03-16 16:52:02 -0400

    Add Starting Side and Verification

--- Display.py
+++ Display.py
@@ -1,7 +1,7 @@
 import chess
 import chess.svg
 
-from PyQt6.QtWidgets import QWidget
+from PyQt6.QtWidgets import QWidget, QLineEdit
 from PyQt6.QtSvgWidgets import QSvgWidget
 from PyQt6.QtGui import QShortcut, QKeySequence
 
@@ -9,6 +9,7 @@ class Display(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Chess")
+
         
         # initialize window: top-left corner 1000 x 1000
         self.setGeometry(0, 0, 1000, 1000)
@@ -16,13 +17,15 @@ class Display(QWidget):
         self.widgetSvg = QSvgWidget(parent = self)
         self.widgetSvg.setGeometry(0, 0, 1000, 1000)
 
-        self.chessboard = chess.Board()
-        self.chessboardSvg = chess.svg.board(self.chessboard).encode("UTF-8") 
-        self.widgetSvg.load(self.chessboardSvg)
         self.shortcuts()
-
+        
+    
     def shortcuts(self):
+        '''Shortcuts for Window
+        <Ctrl + W> close window
+        '''
         # close window
         self.shortcut_close = QShortcut(QKeySequence('Ctrl+W'), self)
         self.shortcut_close.activated.connect(self.close)
+
 
--- play.py
+++ play.py
@@ -6,10 +6,20 @@ from PyQt6.QtWidgets import QApplication
 
 
 def main():
-    # Prompt for FEN
-    FEN = input('Press Enter to Begin or Add a Valid FEN: ')
+    # Prompt for starting color (default white)
+    starting_color = input('Select (W)hite or (B)lack: ')
 
-    # FEN Validation
+    # Validation (starting color)
+    if starting_color and starting_color[0].lower() == "b":
+        starting_color = True
+    else:
+        starting_color = False
+
+
+    # Prompt for FEN
+    FEN = input('Press <Enter> to Begin or Input FEN: ')
+
+    # Validation (FEN)
     try:
         board = chess.Board(FEN)
     except:
@@ -17,13 +27,12 @@ def main():
 
 
     app = QApplication([])
+    chessboardSvg = chess.svg.board(board, flipped = starting_color).encode("UTF-8")
     window = Display()
-    chessboardSvg = chess.svg.board(board, flipped = True).encode("UTF-8")
     window.widgetSvg.load(chessboardSvg)
     window.show()
     app.processEvents()
     sys.exit(app.exec())
-
 
 
 if __name__ == "__main__":
